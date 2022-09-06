19e59e6cf77b7fbf0463f1588b22d154f663c2f1
GrayfienHalim <grayfien2002@gmail.com>
2022-09-06 14:28:21 +0700

    Upload Github

---
+++ app/src/main/java/com/grayfien/testugd1/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.grayfien.testugd1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

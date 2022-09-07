f3f338298eb1442960c89ee78a6ad1255ff6349a
elangsmdr <elangs1911@gmail.com>
2022-09-08 00:50:48 +0700

    RVadapter

--- app/src/main/java/com/grayfien/testugd1/RVPasienAdapter.kt
+++ app/src/main/java/com/grayfien/testugd1/RVPasienAdapter.kt
@@ -17,7 +17,7 @@ class RVPasienAdapter(private val data:Array<Pasien>) : RecyclerView.Adapter<RVP
     override fun onBindViewHolder(holder: viewHolder, position: Int) {
         val currentItem = data[position]
         holder.tvNamaPasien.text = currentItem.name
-        holder.tvJenisKelamin.text = currentItem.jenisKelamin
+        holder.tvTglLahir.text = currentItem.tglLahir
         holder.tvEmail.text = currentItem.email
         holder.tvNomorTelepon.text = currentItem.noTelp
 
@@ -29,8 +29,8 @@ class RVPasienAdapter(private val data:Array<Pasien>) : RecyclerView.Adapter<RVP
 
     class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val tvNamaPasien : TextView = itemView.findViewById(R.id.tv_nama_pasien)
-        val tvJenisKelamin : TextView = itemView.findViewById(R.id.tv_jenis_kelamin)
-        val tvEmail : TextView = itemView.findViewById(R.id.tv_nama_pasien)
+        val tvTglLahir : TextView = itemView.findViewById(R.id.tv_tgl_lahir)
+        val tvEmail : TextView = itemView.findViewById(R.id.tv_email)
         val tvNomorTelepon : TextView = itemView.findViewById(R.id.tv_nomor_telepon)
     }
 }

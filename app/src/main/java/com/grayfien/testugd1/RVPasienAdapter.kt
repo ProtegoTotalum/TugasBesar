0c41268ed8a8a24a8e5eea940eab54ece1c5535e
elangsmdr <elangs1911@gmail.com>
2022-09-08 00:22:00 +0700

    RVadapter

--- app/src/main/java/com/grayfien/testugd1/RVPasienAdapter.kt
+++ app/src/main/java/com/grayfien/testugd1/RVPasienAdapter.kt
@@ -5,8 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-//import com.grayfien.testugd1.entity.Pasien
+import com.grayfien.testugd1.entity.Pasien
 
 class RVPasienAdapter(private val data:Array<Pasien>) : RecyclerView.Adapter<RVPasienAdapter.viewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val  itemView = LayoutInflater.from(parent.context).inflate(R.layout.rv_pasien_adapter, parent, false)
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val currentItem = data[position]
+        holder.tvNamaPasien.text = currentItem.name
+        holder.tvJenisKelamin.text = currentItem.jenisKelamin
+        holder.tvEmail.text = currentItem.email
+        holder.tvNomorTelepon.text = currentItem.noTelp
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val tvNamaPasien : TextView = itemView.findViewById(R.id.tv_nama_pasien)
+        val tvJenisKelamin : TextView = itemView.findViewById(R.id.tv_jenis_kelamin)
+        val tvEmail : TextView = itemView.findViewById(R.id.tv_nama_pasien)
+        val tvNomorTelepon : TextView = itemView.findViewById(R.id.tv_nomor_telepon)
+    }
 }

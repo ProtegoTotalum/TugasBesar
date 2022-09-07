267a32b53d1be80311060d9691caacbaba9eef95
elangsmdr <elangs1911@gmail.com>
2022-09-07 22:59:05 +0700

    RVadapter

---
+++ app/src/main/java/com/grayfien/testugd1/RVPasienAdapter.kt
@@ -0,0 +1,12 @@
+package com.grayfien.testugd1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+//import com.grayfien.testugd1.entity.Pasien
+
+class RVPasienAdapter(private val data:Array<Pasien>) : RecyclerView.Adapter<RVPasienAdapter.viewHolder>() {
+
+}

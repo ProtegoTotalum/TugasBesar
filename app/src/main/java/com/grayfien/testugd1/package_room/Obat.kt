72089562bcfee4566f6dd7bae3441304055e2881
GrayfienHalim <grayfien2002@gmail.com>
2022-09-28 14:57:28 +0700

    Menambahkan notification no 1

---
+++ app/src/main/java/com/grayfien/testugd1/package_room/Obat.kt
@@ -0,0 +1,15 @@
+package com.grayfien.testugd1.package_room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Obat (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val namaObat: String,
+    val jenisObat: String,
+    val deskripsi: String
+)
+
---
+++ app/src/main/java/com/grayfien/testugd1/package_room/ObatDB.kt
@@ -0,0 +1,36 @@
+package com.grayfien.testugd1.package_room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+class ObatDB {
+    @Database(
+        entities = [Pasien::class],
+        version = 1
+    )
+
+    abstract class ObatDB : RoomDatabase(){
+
+        abstract fun obatDao() : ObatDAO
+
+        companion object {
+            @Volatile private var instance : ObatDB? = null
+            private val LOCK = Any()
+
+            operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+                 instance ?: buildDatabase(context).also {
+                    instance = it
+                }
+            }
+
+            private fun buildDatabase(context: Context) =
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    ObatDB::class.java,
+                    "Obat.db"
+                ).build()
+        }
+    }
+}

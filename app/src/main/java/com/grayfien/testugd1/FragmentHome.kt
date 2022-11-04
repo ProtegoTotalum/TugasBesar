b1a9bf6ffec049b94de698ec73ec231c8b08d318
GrayfienHalim <grayfien2002@gmail.com>
2022-11-04 22:14:11 +0700

    Maps

--- app/src/main/java/com/grayfien/testugd1/FragmentHome.kt
+++ app/src/main/java/com/grayfien/testugd1/FragmentHome.kt
@@ -1,10 +1,13 @@
 package com.grayfien.testugd1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_pasien.*
 
 class FragmentHome : Fragment() {
 
@@ -14,6 +17,16 @@ class FragmentHome : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
+
+    fun setupListener() {
+        btnLocation.setOnClickListener { startActivity(
+            Intent(requireActivity(), LocationActivity::class.java)) }
     }
 
 }
